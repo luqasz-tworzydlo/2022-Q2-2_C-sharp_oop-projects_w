4f7b84a7de77d1072d5e6eeab9fbcb1e83f4f2aa
luqasz-tworzydlo <72106587+luqasz-tworzydlo@users.noreply.github.com>
2023-06-04 20:39:44 +0200

    Add files via upload

---
+++ FK1_DzieleniePrzezZero/src/FK_DzieleniePrzezZero/FK_DzieleniePrzezZero/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FK_DzieleniePrzezZero
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 12;
+
+            Console.WriteLine("Twoje wartości to: \n=> x = " + x + "\n=> y = "+ y);
+            Console.WriteLine("\nDziałanie matematyczne to: z = y / x\n");
+
+            try
+            {
+                int z = y / x;
+                Console.WriteLine("x=" + x);
+                Console.WriteLine("y=" + y);
+                Console.WriteLine("z=" + z);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Przykro mi, ale nie można dzielić przez zero :<");
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public class NaszaTablica
+    {
+        int[] tablica;
+        public NaszaTablica()
+        { tablica = new int[5]; }
+        public int getElement(int index)
+        {
+            return tablica[index];
+        }
+    }
+}
