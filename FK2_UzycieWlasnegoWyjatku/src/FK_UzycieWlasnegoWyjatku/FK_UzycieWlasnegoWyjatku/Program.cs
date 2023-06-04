9d4728140e17b28b730125662660641a958d4596
luqasz-tworzydlo <72106587+luqasz-tworzydlo@users.noreply.github.com>
2023-06-04 20:40:44 +0200

    Add files via upload

---
+++ FK2_UzycieWlasnegoWyjatku/src/FK_UzycieWlasnegoWyjatku/FK_UzycieWlasnegoWyjatku/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FK_UzycieWlasnegoWyjatku
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Tablica tab = new Tablica();
+            try
+            {
+                int value = tab.getElement(24);
+                Console.WriteLine("=> Element nr 24 ma wartość: " + value);
+            }
+
+            catch (NieMaTakiegoElementu)
+            {
+                Console.WriteLine("=> Nie ma 'Element nr 24 ma wartość'");
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public class NieMaTakiegoElementu : Exception
+    { }
+
+    public class Tablica
+    {
+        int[] tablica;
+        public Tablica()
+        { tablica = new int[5]; }
+        public int getElement(int index)
+        {
+            if (index < 0 || index > tablica.Length - 1)
+            {
+                throw new NieMaTakiegoElementu();
+            }
+            else
+            {
+
+                return tablica[index];
+            }
+        }
+    }
+}
